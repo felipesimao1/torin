0ffe53be142c9eea858e83b45f263fb1e5e30733
Felipe Simao <31260554+felipesimao1@users.noreply.github.com>
2023-08-03 14:32:31 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 import os
 import json
-from flask import Flask, render_template, request, flash
-from jinja2 import escape
-if os.path.exists("env.py"):
+
+if os. path.exists("env.py"):
     import env
 
 
